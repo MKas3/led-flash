41dfab54eea5e7d40d7e0732b33fa887aaee57ae
mkas3 <madTwo.19@yandex.ru>
2024-08-30 15:59:52 +0300

    feat: new article layout

--- app/blog/[article]/layout.tsx
+++ app/blog/[article]/layout.tsx
@@ -1,19 +1,47 @@
 import React from 'react';
 
+import { FooterForm } from '@/app/_components/footer/footer-form';
+import { Button } from '@/components/ui/button';
+import { Collapsible, CollapsibleContent, CollapsibleTrigger } from '@/components/ui/collapsible';
 import { Container } from '@/components/ui/container';
+import { GradientText } from '@/components/ui/gradient-text';
 import { Main } from '@/components/ui/main';
 
 export default function ArticlesLayout({
-  children,
+  children
 }: {
   children?: React.ReactNode;
 }) {
   return (
-    <Main>
-      <Container className='mb-36' isHero>
-        <div className='relative px-[2.5%] pt-[2.5vw] md:px-[10%] md:pt-[7.5vw] lg:px-[15%] lg:pt-[10vw] 2xl:px-[25%] 2xl:pt-[12.5vw]'>
-          {children}
-        </div>
+    <Main className='w-auto'>
+      <Container className='grid grid-cols-1 gap-y-6 !pt-0 lg:grid-cols-[minmax(0,2.5fr)_minmax(0,1fr)] lg:gap-x-16' isHero>
+        {children}
+        <Collapsible className='group sticky top-[calc(var(--header-height)+2rem)] col-start-2 h-fit'>
+          <div className='flex flex-col items-center justify-center gap-y-6 group-data-[state=open]:hidden'>
+            <span className='text-center text-2xl font-bold uppercase'>
+              Готовы создать красоту для
+              {' '}
+              <GradientText>вашего бизнеса?</GradientText>
+            </span>
+            <CollapsibleTrigger asChild>
+              <Button size='medium' variant='gradient'>Оформить заявку</Button>
+            </CollapsibleTrigger>
+          </div>
+          <CollapsibleContent className='flex flex-col items-center justify-center gap-y-6'>
+            <span className='text-center text-2xl font-bold uppercase'>
+              Заказать
+              {' '}
+              <GradientText>неоновую</GradientText>
+              {' '}
+              вывеску
+            </span>
+            <FooterForm
+              className='w-full grow-0 text-center'
+              size='sm'
+              variant='muted'
+            />
+          </CollapsibleContent>
+        </Collapsible>
       </Container>
     </Main>
   );

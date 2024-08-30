825e0284e166c8530de932b61c805b6a5b9070a4
mkas3 <madTwo.19@yandex.ru>
2024-08-30 15:58:55 +0300

    feat: remove mouse follower & add yandex map provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,97 +1,102 @@
 import type { Metadata, Viewport } from 'next';
 
-import { Poppins, Unbounded } from 'next/font/google';
+import React from 'react';
 
-import { siteConfig } from '@/config/site';
-import { cn } from '@/lib/utils';
-import { Toaster } from '@/components/ui/sonner';
 import { Footer } from '@/app/_components/footer/footer';
-import { Header } from '@/app/_components/header/header';
+import { MotionHeader } from '@/app/_components/header/motion-header';
 import { LenisProvider } from '@/app/_components/lenis-provider';
-import { MouseFollower } from '@/app/_components/mouse-follower';
 import { ScreenIndicator } from '@/app/_components/screen-indicator';
 import { SplashScreenWrapper } from '@/app/_components/splash-screen-wrapper';
+import { YandexMapProvider } from '@/components/shared/yandex-map-provider';
+import { Toaster } from '@/components/ui/sonner';
+import { api } from '@/config/api';
+import { siteConfig } from '@/config/site';
+import { Poppins, Unbounded } from 'next/font/google';
+
+import { cn } from '@/lib/utils';
 
 import './globals.css';
 
 const unbounded = Unbounded({
   subsets: ['latin'],
-  weight: ['400', '500', '600', '700', '800'],
   variable: '--font-unbounded',
+  weight: ['400', '500', '600', '700', '800']
 });
 const poppins = Poppins({
   subsets: ['latin'],
-  weight: ['400', '500', '600', '700', '800'],
   variable: '--font-poppins',
+  weight: ['400', '500', '600', '700', '800']
 });
 
 export const metadata: Metadata = {
-  title: {
-    default: siteConfig.naming,
-    template: `%s - ${siteConfig.naming}`,
-  },
-  metadataBase: new URL(siteConfig.url),
   description: siteConfig.description,
-  keywords: ['led'],
+  icons: {
+    apple: '/apple-touch-icon.png',
+    icon: '/favicon.ico',
+    shortcut: '/favicon.ico'
+  },
+  keywords: ['led', 'led-flash', 'neon'],
+  manifest: '/manifest.webmanifest',
+  metadataBase: new URL(siteConfig.url),
   openGraph: {
-    type: 'website',
-    locale: 'ru_RU',
-    url: siteConfig.url,
-    title: siteConfig.naming,
     description: siteConfig.description,
     images: [
       {
-        url: siteConfig.ogImage,
-        width: 847,
-        height: 443,
         alt: siteConfig.naming,
-      },
+        height: 443,
+        url: siteConfig.ogImage,
+        width: 847
+      }
     ],
+    locale: 'ru_RU',
+    title: siteConfig.naming,
+    type: 'website',
+    url: siteConfig.url
+  },
+  title: {
+    default: siteConfig.naming,
+    template: `%s - ${siteConfig.naming}`
   },
   twitter: {
     card: 'summary_large_image',
-    title: siteConfig.naming,
     description: siteConfig.description,
     images: [siteConfig.ogImage],
-  },
-  icons: {
-    icon: '/favicon.ico',
-    shortcut: '/favicon-16x16.png',
-    apple: '/apple-touch-icon.png',
-  },
-  manifest: `${siteConfig.url}/site.webmanifest`,
+    title: siteConfig.naming
+  }
 };
 
 export const viewport: Viewport = {
-  themeColor: 'black',
-  minimumScale: 1,
   maximumScale: 1,
-  userScalable: false,
+  minimumScale: 1,
+  themeColor: 'black',
+  userScalable: false
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+  children
+}: {
+  children?: React.ReactNode;
+}) {
   return (
     <html lang='en' suppressHydrationWarning>
       <body
         className={cn(
-          'min-h-screen w-screen overflow-x-hidden bg-foreground text-foreground antialiased [&[data-scroll-locked]]:!overflow-y-auto',
+          'min-h-screen group/loading w-screen overflow-x-hidden bg-foreground text-foreground antialiased [&[data-scroll-locked]]:!overflow-y-auto',
           unbounded.variable,
           poppins.variable,
           unbounded.className
         )}
       >
-        <LenisProvider>
-          <SplashScreenWrapper>
-            <Header />
-            {children}
-            <Footer />
-          </SplashScreenWrapper>
+        <LenisProvider root>
+          <YandexMapProvider apiUrl={api.yMapsApiUrl}>
+            <SplashScreenWrapper>
+              <MotionHeader />
+              {children}
+              <Footer />
+            </SplashScreenWrapper>
+          </YandexMapProvider>
           <Toaster />
-          <MouseFollower />
+          {/* <MouseFollower /> */}
           <ScreenIndicator />
         </LenisProvider>
       </body>

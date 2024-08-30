c354ac07087c773e49b6fd82a08ec5918b6f655f
mkas3 <madTwo.19@yandex.ru>
2024-08-30 15:52:38 +0300

    format: lib

--- lib/env.ts
+++ lib/env.ts
@@ -1,19 +1,26 @@
 import { z } from 'zod';
 
+import ProcessEnv = NodeJS.ProcessEnv;
+
 const environmentSchema = z.object({
-  YMAP_API_KEY: z.string(),
+  ANALYZE: z.coerce.boolean().optional(),
+  YMAP_API_KEY: z.string()
 });
 
-const { YMAP_API_KEY } = process.env;
-
-const parsedEnvironment = environmentSchema.parse({
-  YMAP_API_KEY,
-});
+// eslint-disable-next-line node/prefer-global/process
+const { ANALYZE, YMAP_API_KEY } = process.env;
 
 type Environment = z.infer<typeof environmentSchema>;
 
+export const env = environmentSchema.parse({
+  ANALYZE,
+  YMAP_API_KEY
+}) as Environment & ProcessEnv;
+
 declare global {
+  // eslint-disable-next-line ts/no-namespace
   namespace NodeJS {
+    // eslint-disable-next-line ts/consistent-type-definitions
     interface ProcessEnv extends Environment {}
   }
 }
--- lib/utils.ts
+++ lib/utils.ts
@@ -3,9 +3,9 @@ import type { ClassValue } from 'clsx';
 import { clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
-export function cn(...inputs: ClassValue[]) {
+export const cn = (...inputs: ClassValue[]) => {
   return twMerge(clsx(inputs));
-}
+};
 
 export const toHSLString = (hsl: [number, number, number]) =>
   `hsl(${hsl.map((value, index) => (index >= 1 ? `${value}%` : value)).join(',')})`;

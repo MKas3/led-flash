58253377ff0cd71e8e91d5867eb1384c1a61094f
mkas3 <madTwo.19@yandex.ru>
2024-09-09 13:41:52 +0300

    feat: remove article

--- config/blog/articles.ts
+++ config/blog/articles.ts
@@ -1,4 +1,3 @@
-import HowToTransformYourBakery from '@/public/blog/articles/how-to-transform-your-bakery.mdx';
 import SmartLightingControlDimmersAndControllersForNeonSigns from '@/public/blog/articles/smart-lighting-control-dimmers-and-controllers-for-neon-signs.mdx';
 
 export const articlesMetadata = [
@@ -9,17 +8,9 @@ export const articlesMetadata = [
     tag: 'Популярное',
     urlNaming: 'smart-lighting-control-dimmers-and-controllers-for-neon-signs',
     views: '452'
-  },
-  {
-    naming: 'Как преобразить вашу пекарню?',
-    previewSrc: '/blog/articles/preview/how-to-transform-your-bakery.webp',
-    tag: 'Новое',
-    urlNaming: 'how-to-transform-your-bakery',
-    views: '118'
   }
 ];
 
 export const articles: Record<string, any> = {
-  'how-to-transform-your-bakery': HowToTransformYourBakery,
   'smart-lighting-control-dimmers-and-controllers-for-neon-signs': SmartLightingControlDimmersAndControllersForNeonSigns
 };
